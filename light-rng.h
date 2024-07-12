487cd371a2c8e3089ada6e5718656569290a1037
NiKo Kompella <63625756+infection-tag@users.noreply.github.com>
2024-07-12 13:38:14 -0700

    Update light-rng.h

--- light-rng.h
+++ light-rng.h
@@ -1,5 +1,42 @@
-int randomizer(int);
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifndef TRUE
+    #define true !0
+    #define false 0
+#endif
+
+int randomizer(int); /* generate random number from number of digits */
+int pi_crawler(int); /* crawls the first million digits of pi for a 3 or 4 digit query */
+
+int __digit_counter(int); /* finds the number of digits */
+char* __pi_indexer(); /* turns pi file into char array - wildly innefficient */
+
+pi = __pi_indexer();
 
 int randomizer(int digits) { /* generate random number from number of digits */
-  /* under construction */
+    /* under construction */
+}
+
+int pi_crawler(int query) { /* crawls the first million digits of pi for a 3 or 4 digit query */
+    int query_digits = __digit_counter(query);
+    for(int i = 0; 
+}
+
+int __digit_counter(int n) { /* finds the number of digits */
+    if (n < 0) return numPlaces ((n == INT_MIN) ? INT_MAX: -n);
+    if (n < 10) return 1;
+    return 1 + numPlaces (n / 10);
+}
+
+char* __pi_indexer() {
+    FILE* PI_FILE = fopen("pi.txt", "r");
+    char ret[1000000];   
+    int i;
+
+    for(i = 0; i < 1000000; i++)
+        fscanf(PI_FILE, "%c", ret[i]);
+
+    return ret;
 }
